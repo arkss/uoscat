6b1907c1106f59ca9e045de7d7de4c5d707ec724
lsb918 <lsb918@uos.ac.kr>
2019-03-01 16:12:19 +0900

    상속, home html

--- uoscat/urls.py
+++ uoscat/urls.py
@@ -10,4 +10,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',postapp.views.home, name='home'),
-]
+] +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
