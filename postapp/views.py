e311f15c61b5ba16e722fd955c653c4bb13544ed
minsung <rkdalstjd9@naver.com>
2019-03-15 21:47:10 +0900

    #1 투표 1등이 고양이 이름으로

--- postapp/views.py
+++ postapp/views.py
@@ -39,8 +39,7 @@ def newcat(request):
 # 각 고양이의 상세페이지
 def detail(request,num):
     cat=Cat.objects.get(pk=num)
-    # 해당 고양이의 vote 의 id 에 일치하는 고양이 후보이름만 가져온다.
-    choices = Choice.objects.filter(vote_id=cat.vote.id)
+    
     # vote가 없을 경우 예외 처리
     try:
         vote = Vote.objects.get(cat_id=num)
@@ -48,7 +47,8 @@ def detail(request,num):
     except:
         vote = Vote(cat_id=num)
         vote.save()
-
+    # 해당 고양이의 vote 의 id 에 일치하는 고양이 후보이름만 가져온다.
+    choices = Choice.objects.filter(vote_id=cat.vote.id)
 
     context={
         'cat': cat,
@@ -78,7 +78,7 @@ def addhabitat(request,num):
     return redirect('detail',str(num))
 
 # 고양이의 이름 투표 기능
-def vote(request,num):
+def vote_condition(request,num):
     cat = Cat.objects.get(pk=num)
     
     # 투표중인지 표시
@@ -86,10 +86,22 @@ def vote(request,num):
     # 투표를 종료하면 기존의 이름도 데이터에서 삭제해준다. 나중에는 그냥 투표자체를 없애는 걸로 바꾸자.
     if cat.voting:
         cat.voting = False
+        # 투표를 종료하면 고양이의 이름을 투표의 1등으로 바꿔준다.
+        choices = Choice.objects.filter(vote_id=cat.vote.id)
+        max_count = 0
+        for choice in choices:
+            if max_count < choice.count:
+                max_count = choice.count
+        new_name = Choice.objects.get(count = max_count)
+        cat.name = str(new_name)
+        
+
+        vote = Vote.objects.get(cat_id=cat.id).delete()
+       
         # 값을 지울 때는 위에 두 줄이 아닌 아래 한 줄 처럼 해야한다. 왜그럴까...?
         # delete_name = Choice.objects.get(vote_id=cat.vote.id, name=cat.name)
         # delete_name.delete()
-        Choice.objects.get(vote_id=cat.vote.id, name=cat.name).delete()
+        # Choice.objects.get(vote_id=cat.vote.id, name=cat.name).delete()
         
     # 투표를 시작하면 voting을 True로 바꿔주고 현재 고양이의 이름도 투표 목록에 넣어준다.
     else:
@@ -97,10 +109,10 @@ def vote(request,num):
         origin_name = Choice(vote_id=cat.vote.id, name=cat.name, count = 0)
         origin_name.save()
 
-    choices = Choice.objects.all()
+    # choices = Choice.objects.all()
     cat.save()
     return redirect('/detail/'+str(cat.pk))
-    # return render(request, '/detail/'+str(cat.pk))
+    
     
 def vote(request, vote_id):
     
